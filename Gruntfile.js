219dc690597271e452dcf1c0562d0f1642eae032
dnelson <dnelson@devel-dnelson.sce.englandlogistics.com>
2013-04-12 19:01:46 -0400

    Remove dist contents and put static files in the build directory

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,12 +16,12 @@ module.exports = function(grunt) {
           compile: {
             options: {
               data: {
-                stylesheet: grunt.file.read('build/style.css'),
-                javascript: grunt.file.read('build/app.js'),
+                stylesheet: "",//grunt.file.read('build/style.css'),
+                javascript: ""//grunt.file.read('build/app.js'),
               }
             },
             files: {
-              "build/index.html": "source/index.jade"
+              "build/dist/index.html": "source/index.jade"
             }
           }
         },
@@ -32,36 +32,44 @@ module.exports = function(grunt) {
                     //paths: ["source/styles/"]
                 },
                 files: {
-                    "source/styles/style.css": "source/styles/style.less"
+                    "build/temp/style.css": "source/styles/style.less"
                 }
             }
         },
         concat: {
             basic_and_extras: {
                 files: {
-                    'build/style.css': ['source/styles/style.css','source/styles/hljs-github.css','source/styles/jquery.tweet.css'],
-                    'build/app.js': ['source/lib/vendor/hilight.min.js', 'source/lib/vendor/jquery.tweet.js', 'source/lib/app.js']
+                    'build/dist/style.css': ['build/temp/style.css','source/styles/hljs-github.css','source/styles/jquery.tweet.css'],
+                    'build/dist/app.js': ['source/lib/vendor/hilight.min.js', 'source/lib/vendor/jquery.tweet.js', 'source/lib/app.js']
                 }
+            }
+        },
+        copy: {
+            main: {
+                files: [
+                    {expand: true, cwd: 'build/static/', src: ['**'], dest: 'build/dist/'}
+                ]
             }
         },
         connect: {
               server: {
                     options: {
                           port: 9001,
-                          base: 'build/',
+                          base: 'build/dist/',
                           hostname: '*',
                           keepalive:true
                     }
               }
-        },
+        }
     });
 
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-jade');
     grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-copy');
 
     // Default task(s).
-    grunt.registerTask('default', ['less:development', 'concat', 'jade', 'connect']);
+    grunt.registerTask('default', ['less:development', 'concat', 'jade', 'copy', 'connect']);
 
 };
